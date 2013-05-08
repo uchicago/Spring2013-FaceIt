1772c18221790b6d755e72371a08a6a4dcda8ff1
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-05-08 15:31:55 -0500

    Initial Commit

---
+++ FaceIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FaceIt
+//
+//  Created by T. Andrew Binkowski on 5/8/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FaceIt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FaceIt
+//
+//  Created by T. Andrew Binkowski on 5/8/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
